af6539cf41f56f410d7e002c1175b924461fb012
ThiagoThomas09 <thiagothomas09@outlook.com>
2024-07-31 21:37:30 -0300

    Exercises 1.8

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,5 +1,17 @@
 import { useState } from 'react'
 
+const Header = () => {
+  return (
+    <h2>give feedback</h2>
+  )
+}
+
+const Content = () => {
+  return (
+    <h2>statistics</h2>
+  )
+}
+
 const Button = ({ handleClick, text}) => (
   <button onClick={handleClick}>{text}</button>
 )
@@ -7,6 +19,24 @@ const Button = ({ handleClick, text}) => (
 const Display = ({ text, value }) => (
   <p>{text} {value}</p>
 )
+
+const Statistics = (props) => {
+  const total = props.good + props.neutral + props.bad;
+  const average = total > 0 ? (props.good-props.bad) / total : 0;
+  const positive = total > 0 ? (props.good * 100) / total : 0;
+
+  return (
+    <div>
+      <Display text="good" value={props.good} />
+      <Display text="neutral" value={props.neutral} />
+      <Display text="bad" value={props.bad} />
+      <Display text="all" value={total} />
+      <Display text="average" value={average} />
+      <Display text="positive" value={`${positive} %`} />
+    </div>
+  )
+
+}
 
 const App = () => {
   // save clicks of each button to its own state
@@ -16,18 +46,12 @@ const App = () => {
 
   return (
     <div>
-      <h2>give feedback</h2>
+      <Header />
       <Button handleClick={()=>setGood(good+1)} text="good" />
       <Button handleClick={()=>setNeutral(neutral+1)} text="neutral" />
       <Button handleClick={()=>setBad(bad+1)} text="bad" />
-      <h2>statistics</h2>
-      <Display text="good" value={good} />
-      <Display text="neutral" value={neutral} />
-      <Display text="bad" value={bad} />
-      <Display text="all" value={good+neutral+bad} />
-      <Display text="average" value={(good-bad) / (good+neutral+bad)} />
-      <Display text="positive" value={`${((good * 100) / (good+neutral+bad))} %`} />
-
+      <Content />
+      <Statistics good={good} neutral={neutral} bad={bad}/>
 
     </div>
     
