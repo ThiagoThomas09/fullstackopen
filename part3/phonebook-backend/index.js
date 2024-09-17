dd836f19faba59300e44de4290ef55c2e85b6d96
ThiagoThomas09 <thiagothomas09@outlook.com>
2024-09-17 20:41:03 -0300

    Exercise 3.4

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 
-const persons = [
+let persons = [
     { 
       "id": "1",
       "name": "Arto Hellas", 
@@ -38,6 +38,13 @@ app.get('/api/persons/:id', (req, res) => {
   } else {
     res.status(404).end()
   }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = req.params.id
+  persons = persons.filter(person => person.id !== id)
+
+  res.status(204).end()
 })
 
 app.get('/info', (req, res) => {
