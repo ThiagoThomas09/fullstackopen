9f74e0ff4ddc639c495deac7bc40125861e77a5f
ThiagoThomas09 <thiagothomas09@outlook.com>
2024-09-28 16:13:06 -0300

    Exercise 3.9

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const cors = require('cors')
 
 app.use(express.json())
 // app.use(morgan('tiny'))
@@ -9,6 +10,7 @@ morgan.token('body', (req) => {
 })
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use(cors())
 
 let persons = [
     { 
